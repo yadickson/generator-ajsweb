1596c0fefba74e0ff1ae26646722265fb19ca079
Yadickson Soto <yadickson.soto@basedos.cl>
2017-11-07 15:36:49 -0300

    Update service

--- generators/service/templates/service_test.js
+++ generators/service/templates/service_test.js
@@ -4,20 +4,20 @@
       describe('Service:<%= name %>', function() {
           describe('Test module and service', function() {
 
-              // load the controller's module
+              // load the module
               beforeEach(angular.mock.module('<%= projectModule %>'));
 
               var <%= name %>;
-              // Initialize the controller and a mock scope
+              // Initialize the service
               beforeEach(inject(function(_<%= name %>_) {
                   <%= name %> = _<%= name %>_;
               }));
 
-              it('Check service', function() {
+              it('Check service <%= name %>', function() {
                   expect(!!<%= name %>).to.be.true;
               });
 
-              it('Check service.name', function() {
+              it('Check <%= name %>.name', function() {
                   expect(<%= name %>.name).to.be.equal('<%= name %>');
               });
           });
