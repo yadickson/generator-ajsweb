be80e46512b8eaba6030c9f187efc86b886977ab
Yadickson Soto <yadickson.soto@basedos.cl>
2017-11-13 19:41:43 -0300

    Filter support

--- generators/filter/index.js
+++ generators/filter/index.js
@@ -1,16 +1,12 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const camelize = require('camelize');
-const decamelize = require('decamelize');
 
 module.exports = class extends Generator {
 
     constructor(args, opts) {
         super(args, opts);
 
-        this.argument('name', {
+        this.argument('filtername', {
             type: String,
             required: true,
             desc: '[Filter name]'
@@ -20,49 +16,14 @@ module.exports = class extends Generator {
             desc: 'Disable yosay console (default: false)'
         });
 
-        this.name = this.options.name || 'none';
+        this.name = this.options.filtername;
         this.console = !this.options.disableConsole;
-
-        this.varname = camelize(this.name);
-        this.filtername = this.varname + 'Filter';
-        this.file = decamelize(camelize(this.filtername)) + '.js';
-        this.filetest = decamelize(camelize(this.filtername)) + '_test.js';
-
-        this.projectModule = this.config.get('projectModule');
-        this.license = this.config.get('license');
     }
 
-    prompting() {
-        var text = 'Making the filter ' + chalk.blue(this.name);
-        this.log(this.console ? yosay(text) : text);
-    }
-
-    writing() {
-        this._writingFilter();
-        this._writingTest();
-    }
-
-    _writingFilter() {
-        this.fs.copyTpl(
-            this.templatePath('filter.js'),
-            this.destinationPath('app/scripts/filters/' + this.file), {
-                varname: this.varname,
-                name: this.filtername,
-                projectModule: this.projectModule,
-                license: this.license
-            }
-        );
-    }
-
-    _writingTest() {
-        this.fs.copyTpl(
-            this.templatePath('filter_test.js'),
-            this.destinationPath('test/spec/filters/' + this.filetest), {
-                varname: this.varname,
-                name: this.filtername,
-                projectModule: this.projectModule,
-                license: this.license
-            }
-        );
+    install() {
+        this.composeWith(require.resolve('generator-ajsbase/generators/filter'), {
+            arguments: [this.name, 'app/scripts'],
+            disableConsole: !this.console
+        });
     }
 };
