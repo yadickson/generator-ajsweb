0fa91c9f280edc14b8311cf363c86da097baf07a
Yadickson Soto <yadickson.soto@basedos.cl>
2017-11-13 19:42:33 -0300

    Provider support

--- generators/provider/index.js
+++ generators/provider/index.js
@@ -1,16 +1,12 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const camelize = require('camelize');
-const decamelize = require('decamelize');
 
 module.exports = class extends Generator {
 
     constructor(args, opts) {
         super(args, opts);
 
-        this.argument('name', {
+        this.argument('providername', {
             type: String,
             required: true,
             desc: '[Provider name]'
@@ -20,49 +16,14 @@ module.exports = class extends Generator {
             desc: 'Disable yosay console (default: false)'
         });
 
-        this.name = this.options.name || 'none';
+        this.name = this.options.providername;
         this.console = !this.options.disableConsole;
-
-        this.varname = camelize(this.name);
-        this.providername = this.varname + 'Provider';
-        this.file = decamelize(camelize(this.providername)) + '.js';
-        this.filetest = decamelize(camelize(this.providername)) + '_test.js';
-
-        this.projectModule = this.config.get('projectModule');
-        this.license = this.config.get('license');
     }
 
-    prompting() {
-        var text = 'Making the provider ' + chalk.blue(this.name);
-        this.log(this.console ? yosay(text) : text);
-    }
-
-    writing() {
-        this._writingProvider();
-        this._writingTest();
-    }
-
-    _writingProvider() {
-        this.fs.copyTpl(
-            this.templatePath('provider.js'),
-            this.destinationPath('app/scripts/services/' + this.file), {
-                varname: this.varname,
-                name: this.providername,
-                projectModule: this.projectModule,
-                license: this.license
-            }
-        );
-    }
-
-    _writingTest() {
-        this.fs.copyTpl(
-            this.templatePath('provider_test.js'),
-            this.destinationPath('test/spec/services/' + this.filetest), {
-                varname: this.varname,
-                name: this.providername,
-                projectModule: this.projectModule,
-                license: this.license
-            }
-        );
+    install() {
+        this.composeWith(require.resolve('generator-ajsbase/generators/provider'), {
+            arguments: [this.name, 'app/scripts'],
+            disableConsole: !this.console
+        });
     }
 };
