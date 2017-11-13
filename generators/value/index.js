549692e7d3303603d9228dbce030dcf708e610b6
Yadickson Soto <yadickson.soto@basedos.cl>
2017-11-13 19:44:03 -0300

    Value support

--- generators/value/index.js
+++ generators/value/index.js
@@ -1,16 +1,12 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const camelize = require('camelize');
-const decamelize = require('decamelize');
 
 module.exports = class extends Generator {
 
     constructor(args, opts) {
         super(args, opts);
 
-        this.argument('name', {
+        this.argument('valuename', {
             type: String,
             required: true,
             desc: '[Value name]'
@@ -20,49 +16,14 @@ module.exports = class extends Generator {
             desc: 'Disable yosay console (default: false)'
         });
 
-        this.name = this.options.name || 'none';
+        this.name = this.options.valuename;
         this.console = !this.options.disableConsole;
-
-        this.varname = camelize(this.name);
-        this.valuename = this.varname + 'Value';
-        this.file = decamelize(this.valuename) + '.js';
-        this.filetest = decamelize(this.valuename) + '_test.js';
-
-        this.projectModule = this.config.get('projectModule');
-        this.license = this.config.get('license');
     }
 
-    prompting() {
-        var text = 'Making the value ' + chalk.blue(this.name);
-        this.log(this.console ? yosay(text) : text);
-    }
-
-    writing() {
-        this._writingValue();
-        this._writingTest();
-    }
-
-    _writingValue() {
-        this.fs.copyTpl(
-            this.templatePath('value.js'),
-            this.destinationPath('app/scripts/services/' + this.file), {
-                varname: this.varname,
-                name: this.valuename,
-                projectModule: this.projectModule,
-                license: this.license
-            }
-        );
-    }
-
-    _writingTest() {
-        this.fs.copyTpl(
-            this.templatePath('value_test.js'),
-            this.destinationPath('test/spec/services/' + this.filetest), {
-                varname: this.varname,
-                name: this.valuename,
-                projectModule: this.projectModule,
-                license: this.license
-            }
-        );
+    install() {
+        this.composeWith(require.resolve('generator-ajsbase/generators/value'), {
+            arguments: [this.name, 'app/scripts'],
+            disableConsole: !this.console
+        });
     }
 };
