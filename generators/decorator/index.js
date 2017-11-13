0fd45e9516249425f5e6de3dfbc0f7e6f386a880
Yadickson Soto <yadickson.soto@basedos.cl>
2017-11-13 19:36:42 -0300

    Decorator support

--- generators/decorator/index.js
+++ generators/decorator/index.js
@@ -1,64 +1,29 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const camelize = require('camelize');
-const decamelize = require('decamelize');
 
 module.exports = class extends Generator {
 
     constructor(args, opts) {
         super(args, opts);
 
-        this.argument('name', {
+        this.argument('decoratorname', {
             type: String,
             required: true,
             desc: '[Decorator name]'
         });
 
-        this.name = this.options.name || 'none';
+        this.option('disableConsole', {
+            desc: 'Disable yosay console (default: false)'
+        });
+
+        this.name = this.options.decoratorname;
         this.console = !this.options.disableConsole;
-
-        this.varname = camelize(this.name);
-        this.decoratorname = this.varname + 'Decorator';
-        this.file = decamelize(camelize(this.decoratorname)) + '.js';
-        this.filetest = decamelize(camelize(this.decoratorname)) + '_test.js';
-
-        this.projectModule = this.config.get('projectModule');
-        this.license = this.config.get('license');
     }
 
-    prompting() {
-        var text = 'Making the decorator ' + chalk.blue(this.name);
-        this.log(this.console ? yosay(text) : text);
-    }
-
-    writing() {
-        this._writingDecorator();
-        this._writingTest();
-    }
-
-    _writingDecorator() {
-        this.fs.copyTpl(
-            this.templatePath('decorator.js'),
-            this.destinationPath('app/scripts/decorators/' + this.file), {
-                varname: this.varname,
-                name: this.decoratorname,
-                projectModule: this.projectModule,
-                license: this.license
-            }
-        );
-    }
-
-    _writingTest() {
-        this.fs.copyTpl(
-            this.templatePath('decorator_test.js'),
-            this.destinationPath('test/spec/decorators/' + this.filetest), {
-                varname: this.varname,
-                name: this.decoratorname,
-                projectModule: this.projectModule,
-                license: this.license
-            }
-        );
+    install() {
+        this.composeWith(require.resolve('generator-ajsbase/generators/decorator'), {
+            arguments: [this.name, 'app/scripts'],
+            disableConsole: !this.console
+        });
     }
 };
