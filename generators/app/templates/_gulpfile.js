00cc19b023e3a85ac7ed84bc15ce6b0470527662
Yadickson Soto <yadickson.soto@basedos.cl>
2018-04-27 15:15:30 -0300

    Update generator to use new gulp-ajsweb

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,203 +1,54 @@
 // generated on <%= date %> using <%= name %> <%= version %>
 
 const gulp = require('gulp');
-const connect = require('gulp-connect');
-const open = require('gulp-open');
-const runSequence = require('run-sequence');
-const karma = require('karma').Server;
-const del = require('del');
-const jshint = require("gulp-jshint");
-const ajsweb = require("gulp-ajsweb");
-const rename = require("gulp-rename");
 
-let dev = true;
-let minimal = false;
-let dest = 'dist';
-let bootstrap = <%= includeBootstrap %>;
-let sass = <%= includeSass %>;
-let addpaths = [];
+let addpaths = [
+  'node_modules/angular/angular.js',
+  'node_modules/angular-animate/angular-animate.js',
+  'node_modules/angular-route/angular-route.js',
+  'node_modules/bootstrap/**/bootstrap.js'
+];
 
-gulp.task('clean', () => {
-    return del(['build', 'dist', 'coverage', 'reports', '*.tgz', '*.zip', 'docs']);
-});
+let addtestpaths = [
+  'node_modules/angular-mocks/angular-mocks.js'
+];
 
-gulp.task('compile', ['scripts', 'styles', 'jshint', 'fonts', 'images', 'icon', 'views'], () => {});
+let excludepaths = [
+  'node_modules/angular/index.js',
+  'node_modules/angular-animate/index.js',
+  'node_modules/angular-route/index.js'
+];
+
+let addcss = [
+];
+
+let addscss = [
+  'node_modules/bootstrap-sass/assets/stylesheets/**/*.scss'
+];
+
+let addfonts = [
+
+];
+
+let orderBy = [
+  '**/jquery.js',
+  '**/angular.js',
+  '**/angular-*.js',
+  '**/bootstrap.js',
+  '**/bootstrap-*.js',
+  '*'
+];
+
+options = {
+  addpaths: addpaths,
+  addtestpaths: addtestpaths,
+  excludepaths: excludepaths,
+  addcss: addcss,
+  addscss: addscss,
+  addfonts: addfonts,
+  orderBy: orderBy
+};
+
+const ajsweb = require("gulp-ajsweb")(gulp, options);
 
 gulp.task('default', ['build'], () => {});
-
-gulp.task('build', () => {
-    return new Promise(resolve => {
-        minimal = false;
-        dest = 'build';
-        runSequence(['clean'], ['compile'], resolve);
-    });
-});
-
-gulp.task('dist', () => {
-    return new Promise(resolve => {
-        minimal = true;
-        dest = 'dist';
-        runSequence(['clean'], ['compile'], resolve);
-    });
-});
-
-gulp.task('docs', () => {
-    return new Promise(resolve => {
-        minimal = true;
-        dest = 'docs';
-        runSequence(['clean'], ['compile'], ['js2docs'], resolve);
-    });
-});
-
-gulp.task('scripts', () => {
-    return ajsweb.buildScripts({
-            dest: dest,
-            minimal: minimal,
-            addpaths: addpaths
-        })
-        .pipe(connect.reload());
-});
-
-gulp.task('styles', () => {
-    return ajsweb.buildStyles({
-            dest: dest,
-            minimal: minimal,
-            bootstrap: bootstrap,
-            sass: sass
-        })
-        .pipe(connect.reload());
-});
-
-gulp.task('jshint', () => {
-    return ajsweb.appScripts()
-        .pipe(jshint())
-        .pipe(jshint.reporter());
-});
-
-gulp.task('fonts', () => {
-    return ajsweb.buildFonts({
-            dest: dest,
-            minimal: minimal
-        })
-        .pipe(connect.reload());
-});
-
-gulp.task('views', () => {
-    return ajsweb.buildViews({
-            dest: dest,
-            minimal: minimal,
-            bootstrap: bootstrap,
-            sass: sass,
-            addpaths: addpaths
-        })
-        .pipe(connect.reload());
-});
-
-gulp.task('images', () => {
-    return ajsweb.buildImages({
-            dest: dest,
-            minimal: minimal
-        })
-        .pipe(connect.reload());
-});
-
-gulp.task('icon', () => {
-    return ajsweb.buildIcon({
-            dest: dest,
-            minimal: minimal
-        })
-        .pipe(connect.reload());
-});
-
-gulp.task('js2docs', function() {
-    return ajsweb.buildDocs({
-        dest: dest
-    });
-});
-
-gulp.task('karma-cnf', () => {
-    return ajsweb.updateKarmaFile({
-        configFile: 'karma.conf.js',
-        dest: '.',
-        addpaths: addpaths
-    });
-});
-
-gulp.task('karma-server', () => {
-    return new karma({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }).start();
-});
-
-gulp.task('testHtml', () => {
-    return ajsweb.buildIndexTest({
-        dest: dest,
-        minimal: minimal,
-        bootstrap: bootstrap,
-        sass: sass,
-        addpaths: addpaths
-    })
-    .pipe(rename('index.html'))
-    .pipe(gulp.dest(dest));
-});
-
-gulp.task('test', function() {
-    return new Promise(resolve => {
-        runSequence(['karma-cnf'], ['build'], ['testHtml'], ['karma-server'], resolve);
-    });
-});
-
-gulp.task('connect', function() {
-    return connect.server({
-        port: 9100,
-        root: [dest],
-        livereload: true
-    });
-});
-
-gulp.task('open', function(){
-    return gulp.src(dest + '/index.html')
-        .pipe(open({
-            uri: 'localhost:9100',
-            app: 'firefox'
-        }
-    ));
-});
-
-gulp.task('watch', function() {
-    gulp.watch(ajsweb.paths.appScripts, ['scripts', 'jshint']);
-    gulp.watch(ajsweb.paths.appStyles, ['styles']);
-    gulp.watch(ajsweb.paths.appViews, ['views']);
-    gulp.watch(ajsweb.paths.appImages, ['images']);
-    gulp.watch(ajsweb.paths.appFonts, ['fonts']);
-    gulp.watch(ajsweb.paths.appIcon, ['icons']);
-});
-
-gulp.task('watchtest', function() {
-    gulp.watch(ajsweb.paths.appTests, ['scripts', 'jshint', 'testHtml']);
-});
-
-gulp.task('serve', function() {
-    return new Promise(resolve => {
-        runSequence(['build'], ['connect'], ['watch'], ['open'], resolve);
-    });
-});
-
-gulp.task('serve:dist', function() {
-    return new Promise(resolve => {
-        runSequence(['dist'], ['connect'], ['open'], resolve);
-    });
-});
-
-gulp.task('serve:test', function() {
-    return new Promise(resolve => {
-        runSequence(['karma-cnf'], ['build'], ['testHtml'], ['connect'], ['watch'], ['watchtest'], ['open'], resolve);
-    });
-});
-
-gulp.task('serve:docs', function() {
-    return new Promise(resolve => {
-        runSequence(['docs'], ['connect'], ['open'], resolve);
-    });
-});
